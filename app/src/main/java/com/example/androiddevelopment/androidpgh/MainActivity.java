232d3e40ee6b0ac39db7bf30f905aca81818b331
BojanBatinic <bojanbatinic77@gmail.com>
2018-03-20 20:55:13 +0100

    k2

--- app/src/main/java/com/example/androiddevelopment/androidpgh/MainActivity.java
+++ app/src/main/java/com/example/androiddevelopment/androidpgh/MainActivity.java
@@ -21,11 +21,13 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.androiddevelopment.androidpgh.db.DataBaseHelper;
+import com.example.androiddevelopment.androidpgh.model.vest;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 
 
@@ -46,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
 
     private DataBaseHelper databaseHelper;
+    public static String VEST_KEY = "VEST_KEY";
+    public static String NOTIF_TOAST = "notif_toast";
+    public static String NOTIF_STATUS = "notif_statis";
 
 
     @Override
@@ -72,10 +77,86 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+        final ListView listView = (ListView) findViewById(R.id.vesti_list);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_launcher_background);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.show();
+        }
+
+        try {
+            List<vest> list = getDatabaseHelper().getmVestDao().queryForAll();
+
+            ListAdapter adapter = new ArrayAdapter<>(MainActivity.this, R.layout.list_item, list);
+            listView.setAdapter(adapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    vest p = (vest) listView.getItemAtPosition(position);
+
+                    Intent intent = new Intent(MainActivity.this, VestActivity.class);
+                    intent.putExtra(VEST_KEY, p.getmId());
+                    startActivity(intent);
+                }
+            });
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
 
 
     }
+
+
+    private void addItem(){
+        vest vest = new vest();
+        vest.setmName("Vest br 1");
+        vest.setDate("20 mart 2018");
+        //vest.setDescription("The apple tree is a deciduous tree in the rose family best known for its sweet, pomaceous fruit, the apple.");
+        //vest.setImage("v1.jpg");
+
+        try {
+            getDatabaseHelper().getmVestDao().create(vest);
+
+            refresh();
+
+            Toast.makeText(this, "Vest ubacena", Toast.LENGTH_SHORT).show();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void refresh() {
+        ListView listview = (ListView) findViewById(R.id.vesti_list);
+
+        if (listview != null){
+            ArrayAdapter<vest> adapter = (ArrayAdapter<vest>) listview.getAdapter();
+
+            if(adapter!= null)
+            {
+                try {
+                    adapter.clear();
+                    List<vest> list = getDatabaseHelper().getmVestDao().queryForAll();
+
+                    adapter.addAll(list);
+
+                    adapter.notifyDataSetChanged();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
 
     public DataBaseHelper getDatabaseHelper() {
         if (databaseHelper == null) {
